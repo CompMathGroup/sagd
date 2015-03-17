c88c4f5c2d6159294ed3410087e5d01b0486818b
MiptEgor <egor-fi@yandex.ru>
2015-03-17 18:43:11 +0300

    Filtration

---
+++ filtration.cpp
@@ -0,0 +1,29 @@
+#include "const.h"
+void K_permeability(double* tetta, double* K_perm) //Проницаемость
+{
+	int m;
+	for (m=1;m<M-1;m++)
+	{
+		K_perm[m]=(tetta[m]*tetta[m])/(1+tetta[m])/(1+tetta[m]);   //Задаю проницаемость как квадрат насыщенности. (На самом деле в тетта лежит отношение насыщеносттей, поэтому такая формула)
+	}
+}
+
+
+void W_filtration(double* tetta, double* K_perm, double* W_fil ) //Вектор Фильтрации. (На границах задаем что он ноль)
+{
+	int m;
+	for (m=1;m<M-2;m++)
+	{
+		W_fil[m]=K_perm[m]/etta*(ro_s-ro_l)*gravity;		//m+1/2
+	}
+}
+
+
+void saturation(double* tetta, double* W_fil, double* tetta_new) // отношение относительных насыщенностей жидкости и скелета
+{
+	int m;
+	for (m=1;m<M-1;m++)
+	{
+		tetta_new[m]=tetta[m]-tau*(W_fil[m]-W_fil[m-1])/h;	
+	}
+}
